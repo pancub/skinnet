33fa944e39a70c12fe2918b6b5a66da3c1c9758d
Srinivasreddytopoji <Srinivasreddy.topoji@gmail.com>
2021-11-16 10:41:09 -0500

    End of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,16 +13,16 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repository;
 
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository repository)
         {
-            _context = context;
+            _repository = repository;
         }
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
+            var products = await _repository.GetProductsAsync();
 
             return Ok(products);
         }
@@ -29,9 +30,26 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<Product> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repository.GetProductByIdAsync(id);
         }
 
+        [HttpGet]
+        [Route("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            var productBrands = await _repository.GetProductBrandsAsync();
+
+            return Ok(productBrands);
+        }
+
+        [HttpGet]
+        [Route("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductTypes()
+        {
+            var productTypes = await _repository.GetProductTypesAsync();
+
+            return Ok(productTypes);
+        }
 
     }
 }
---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext storeContext,ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if(!storeContext.ProductBrands.Any())
+                {
+                    var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+
+                    var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+
+                    foreach (var item in brands)
+                    {
+                        storeContext.ProductBrands.Add(item);
+                    }
+
+                    await storeContext.SaveChangesAsync();
+                }
+
+                if(!storeContext.ProductTypes.Any())
+                {
+                    var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+
+                    var brands = JsonSerializer.Deserialize<List<ProductType>>(brandsData);
+
+                    foreach (var item in brands)
+                    {
+                        storeContext.ProductTypes.Add(item);
+                    }
+
+                    await storeContext.SaveChangesAsync();
+                }
+
+                 if(!storeContext.Products.Any())
+                {
+                    var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+
+                    var brands = JsonSerializer.Deserialize<List<Product>>(brandsData);
+
+                    foreach (var item in brands)
+                    {
+                        storeContext.Products.Add(item);
+                    }
+
+                    await storeContext.SaveChangesAsync();
+                }
+
+            }
+            catch(Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<StoreContextSeed>();
+                logger.LogError(ex,"Error while seeding data.");
+            }
+        }
+    }
+}
